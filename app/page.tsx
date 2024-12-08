e5f91c8c9b1a8a0d871dd25c0f284f9dffb59f0a
Arodrigow <rodrigow_@hotmail.com.br>
2024-12-08 12:23:48 -0300

    Deleted unused text

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import ButtonComponent from "./components/button";
 import LogoComponent from "./components/logoComponent";
 
-const altTextImage = "Imagem da logomarca da Escola Estadual Salmen Bukzem"
-
 export default function Home() {
   return (
     <div className="w-screen h-screen flex justify-center items-center bg-white">
