af6fe20f2d72f9de5b632ccd8d57c62b7a9c337a
Arodrigow <rodrigow_@hotmail.com.br>
2024-12-22 12:15:37 -0300

    Added interactivity to delete button

--- app/components/listUnit.tsx
+++ app/components/listUnit.tsx
@@ -2,10 +2,17 @@ import { ListUnitProps } from "@/types/types";
 import ButtonComponent from "./button";
 
 export default function ListUnit(props: ListUnitProps){
+    const onClickHandler = () =>{
+        const index = props.list.findIndex((value) => value === props.text);
+        let aux: string[] = Array.from(props.list);
+        aux.splice(index,1);
+        props.setList(aux);
+    }
+
     return (
         <div className="flex justify-between gap-4">
             <span className="text-lg font-bold text-school">{props.text}</span>
-            <ButtonComponent text="Apagar" type="delete"></ButtonComponent>
+            <ButtonComponent text="Apagar" type="delete" onClickHandler={onClickHandler}></ButtonComponent>
         </div>
     )
 }
